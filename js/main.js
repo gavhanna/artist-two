ca40c82dfca0763c6ea23c9137ca55183bb391c1
gavhanna <gavhanna@gmail.com>
2016-07-10 15:56:38 +0100

    v1

--- js/main.js
+++ js/main.js
@@ -1,18 +1,38 @@
-(function ($) {
-  $('#panel').scotchPanel({
-      containerSelector: 'body',
-      direction: 'right', // Make it toggle in from the left
-      duration: 300,
-      transition: 'ease-in-out',
-      clickSelector: '.toggle-panel', // Enables toggling when clicking elements of this class
-      distanceX: '60%', // Size of the toggle
-      enableEscapeKey: true // Clicking Esc will close the panel
-  });
+(function($) {
+  // $('#panel').scotchPanel({
+  //   containerSelector: 'body',
+  //   direction: 'right', // Make it toggle in from the left
+  //   duration: 300,
+  //   transition: 'ease-in-out',
+  //   clickSelector: '.toggle-panel', // Enables toggling when clicking elements of this class
+  //   distanceX: '60%', // Size of the toggle
+  //   enableEscapeKey: true // Clicking Esc will close the panel
+  //});
 
-  $(".content-box").hover(function(){
+  $(".content-box").hover(function() {
     $(this).find(".blurb").toggleClass("animate");
     $(this).find(".content-title").toggleClass("animated");
   });
+
+
+  var scotchPanel = $('#panel').scotchPanel({
+        containerSelector: 'body',
+        direction: 'right',
+        duration: 300,
+        transition: 'ease',
+        distanceX: '60%',
+        enableEscapeKey: true,
+    });
+
+    $('.toggle-panel').click(function() {
+        scotchPanel.toggle();
+        return false;
+    });
+
+    $('.overlay').click(function() {
+      // CLOSE ONLY
+      scotchPanel.close();
+    });
 
 
 
